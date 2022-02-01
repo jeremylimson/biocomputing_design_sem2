87a8010152c70f4a68de73ea635db03e78e9504f
jeremiah.limson@marquette.edu <jeremiah.limson@marquette.edu>
2022-02-01 16:32:52 -0600

    test file instantiate

--- lab1/limsonj_stats_test.cpp
+++ lab1/limsonj_stats_test.cpp
@@ -3,7 +3,11 @@
 * jeremy limson
 * description : test file for testing
 */
+#include "limsonj_stats.cpp"
 
-void main(int argc, char argv[]) {
-    
+int main(int argc, char argv[]) {
+    std::vector<float>* test_vector = {1.1, 2.2, 4.4, 5.5, 6.6, 7.7, 8.8, 10.1};
+    myStats::statTools statTest;
+
+    return 0;
 }
