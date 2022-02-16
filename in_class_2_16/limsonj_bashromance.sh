a8a5ae2c88e967dd73e721cd9a35ef8cd780d9d6
jeremiah.limson@marquette.edu <jeremiah.limson@marquette.edu>
2022-02-16 16:35:18 -0600

    fixed bad romance to use if and for

--- in_class_2_16/limsonj_bashromance.sh
+++ in_class_2_16/limsonj_bashromance.sh
@@ -5,22 +5,45 @@
 
 lyric1="Oh-oh-oh-oh-oh, oh-oh-oh-oh, oh-oh-oh"
 lyric2="Caught in a bad romance"
-lyric3="Ra-ra-ah-ah-ah"
-lyric4="Roma Roma-ma"
-lyric5="Gaga, ooh la-la"
-lyric6="Want your bad romance"
+lyric5="Ra-ra-ah-ah-ah"
+lyric6="Roma Roma-ma"
+lyric7="Gaga, ooh la-la"
+lyric8="Want your bad romance"
 
-# print intro to song using for loop and if statements
-for (( i=1;i<=6;i++ ));
+for (( i=1;i<=12;i++ ));
 do
-    var="lyric$i"
-    if [ "lyric$i" = "$lyric1" ];
+    if [ $i -eq 1 ] || [ $i -eq 3 ];
     then
-        echo lyric1
-    elif [ "lyric$i" = "$lyric2" ];
+        echo $lyric1
+    elif [ $i -eq 2 ] || [ $i -eq 4 ];
     then
-        echo lyric2
-    else
-        echo ${!var}
+        echo $lyric2
+    elif [ $i -eq 5 ] || [ $i -eq 9 ];
+    then
+        echo $lyric5
+    elif [ $i -eq 6 ] || [ $i -eq 10 ];
+    then
+        echo $lyric6
+    elif [ $i -eq 7 ] || [ $i -eq 11 ];
+    then
+        echo $lyric7
+    elif [ $i -eq 8 ] || [ $i -eq 12 ];
+    then
+        echo $lyric8
+    fi
+done
+
+file_name=./good_movies.txt
+
+echo $file_name
+
+good_movies=cat $file_name
+
+# print favorite movie from the good_movies.txt list
+for movie in $good_movies;
+do
+    if [ "$movie" = "Nick is a bad coder" ];
+    then
+        echo ${movie}
     fi
 done
