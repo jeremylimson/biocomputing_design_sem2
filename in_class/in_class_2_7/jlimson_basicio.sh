3bb05e97fcc6dfcd2b1abbad675e2577f1572dd0
jeremiah.limson@marquette.edu <jeremiah.limson@marquette.edu>
2022-02-21 11:49:05 -0600

    in_class and mini project

---
+++ in_class/in_class_2_7/jlimson_basicio.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# jeremy limson
+# 2/7/2022
+# in class 2/7 file i/o
+
+touch "agent_smith"
+
+ln -s ./agent_smtih ./me_me
+
+ln -s ./agent_smtih ./me
+
+ln -s ./agent_smtih ./me_too
+
+chmod +rw ./agent_smith
+
+ls -l > ./agent_smith
+
+wc -l > smith_list.txt
